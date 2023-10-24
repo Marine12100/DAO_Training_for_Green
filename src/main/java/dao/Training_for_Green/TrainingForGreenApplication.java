33c8b0d4fe9792f12d0afff738ab8af20fe58b4c
Marine12100 <marinebrioude@gmail.com>
2023-10-24 09:35:43 +0200

    Initial Commit

---
+++ src/main/java/dao/Training_for_Green/TrainingForGreenApplication.java
@@ -0,0 +1,13 @@
+package dao.Training_for_Green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingForGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingForGreenApplication.class, args);
+	}
+
+}
